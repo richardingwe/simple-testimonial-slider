bbc113aebf18195ab8f1bd1aa2ade7f1ba41187e
Richard Ingwe <richardingwe5@gmail.com>
2021-01-06 22:19:17 +0100

    made slider using owl carousel and bootstrap

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+    $(".owl-carousel").owlCarousel({
+
+        items: 4,
+        nav: true,
+        itemsDesktop: [1000, 4],
+        itemsDesktopSmall: [980, 3],
+        itemsTablet: [768, 2],
+        itemsMobile: [650, 1],
+        pagination: true,
+        navigation: false,
+        slideSpeed: 1000,
+        autoPlay: true
+
+
+
+
+
+
+    });
+
+});
