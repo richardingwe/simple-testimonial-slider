c03e6dd12e8ac68a9f388ffd30d6dc2b8510002d
Richard Ingwe <richardingwe5@gmail.com>
2021-01-08 11:04:17 +0100

    created slider with bootstrap alone

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-$(document).ready(function () {
+// $(document).ready(function () {
 
-    $(".owl-carousel").owlCarousel({
+//     $(".owl-carousel").owlCarousel({
 
-        items: 4,
-        nav: true,
-        itemsDesktop: [1000, 4],
-        itemsDesktopSmall: [980, 3],
-        itemsTablet: [768, 2],
-        itemsMobile: [650, 1],
-        pagination: true,
-        navigation: true,
-        slideSpeed: 1000,
-        autoPlay: true,
-        navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"]
-    });
-    // $(".owl-prev").html('<i class="fa fa-chevron-left"></i>');
-    // $(".owl-next").html('<i class="fa fa-chevron-right"></i>');
+//         items: 4,
+//         nav: true,
+//         itemsDesktop: [1000, 4],
+//         itemsDesktopSmall: [980, 3],
+//         itemsTablet: [768, 2],
+//         itemsMobile: [650, 1],
+//         pagination: true,
+//         navigation: true,
+//         slideSpeed: 1000,
+//         autoPlay: true,
+//         navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"]
+//     });
+//     // $(".owl-prev").html('<i class="fa fa-chevron-left"></i>');
+//     // $(".owl-next").html('<i class="fa fa-chevron-right"></i>');
 
-});
+// });
